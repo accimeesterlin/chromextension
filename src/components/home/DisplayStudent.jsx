9bfef913fe7ec1d21eb20edd04b31a86f188c238
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2018-12-25 17:30:55 -0600

    added copy to clip icon and css styling

--- src/components/home/DisplayStudent.jsx
+++ src/components/home/DisplayStudent.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import _ from 'lodash';
+import { FileCopy } from '@material-ui/icons';
+
 
 const DisplayStudent = ({ students, selectStudent }) => {
     return(
         <div className="displayStudents">
             { _.isEmpty(students) ? <p>No students found</p> : students.map((student, i) => (
-                <p key = {i} onClick = {() => selectStudent(student)}> {student.name} </p>
+                <p key = {i} onClick = {() => selectStudent(student)}> {student.name} 
+                <FileCopy className="fileCopy" onClick={() => {}} />
+                </p>
             ))}
         </div>
     );
