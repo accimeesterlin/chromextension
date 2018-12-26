a21bf7b6ec53d340e63e7f44aa000bcec4769b75
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2018-12-26 08:42:08 -0600

    added popup with selections for adp notes

---
+++ src/components/home/DialogSelect.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select'; import { FileCopy } from '@material-ui/icons';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+});
+
+class DialogSelect extends React.Component {
+    state = {
+        open: false,
+        age: '',
+        back2Back: null,
+        noShow: null
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+
+        this.setState({ open: false });
+        navigator.clipboard.writeText(`
+        1.${this.props.classCode}
+        2.${this.props.studentName}
+        3.B2B-${this.state.back2Back}
+        4.${this.state.noShow}
+
+        `).then(() => {
+
+            }
+
+            , () => alert('fail fail fail'));
+
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <div>
+                <FileCopy className="fileCopy" onClick={this.handleClickOpen} />
+                <Dialog
+                    disableBackdropClick
+                    disableEscapeKeyDown
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                >
+                    <DialogTitle>ADP Notes</DialogTitle>
+                    <DialogContent>
+                        <p>1.{this.props.classCode}</p>
+                        <p>2.{this.props.studentName}</p>
+                        <p>3.B2B-{this.state.back2Back}</p>
+                        <p>4.{this.state.noShow}</p>
+                    </DialogContent>
+                    <DialogContent>
+                        <form className={classes.container}>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="age-native-simple">B2B</InputLabel>
+                                <Select
+                                    native
+                                    value={this.state.back2Back}
+                                    onChange={this.handleChange('back2Back')}
+                                    input={<Input id="age-native-simple" />}
+                                >
+                                    <option value="" />
+                                    <option value={'N'}>No</option>
+                                    <option value={'Y'}>Yes</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl className={classes.formControl}>
+                                <InputLabel htmlFor="age-simple">No Show</InputLabel>
+                                <Select
+                                    value={this.state.noShow}
+                                    onChange={this.handleChange('noShow')}
+                                    input={<Input id="age-simple" />}
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={'NO'}>No</MenuItem>
+                                    <MenuItem value={'YES'}>Yes</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+            </Button>
+                        <Button>
+                            <FileCopy className="fileCopy" onClick={this.handleClose} />
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+DialogSelect.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DialogSelect);
--- src/components/home/DisplayStudent.jsx
+++ src/components/home/DisplayStudent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-import { FileCopy } from '@material-ui/icons';
+import DialogSelect from './DialogSelect';
 
 
 class DisplayStudent extends Component {
@@ -12,28 +12,7 @@ class DisplayStudent extends Component {
             students: props.students,
             selectStudent: props.selectStudent,
             open: false,
-            something: null
         }
-
-        this.copyToClip = this.copyToClip.bind(this);
-    }
-
-    copyToClip = (student) => {
-        console.log(student)
-
-        navigator.clipboard.writeText(`
-    1.${student.studentCode}
-    2.${student.name}
-    3. N/A
-    4. N/A 
-    `).then(() =>
-
-                this.setState({
-                    open: true,
-                    something: student.name
-                })
-            , () => alert('fail fail fail'));
-
 
     }
 
@@ -42,14 +21,14 @@ class DisplayStudent extends Component {
         return (
             <div className="displayStudents">
                 {_.isEmpty(this.state.students) ? <p>No students found</p> : this.state.students.map((student, i) => (
-                    <div className='student-list'>
-                        <p key={i} onClick={() => this.state.selectStudent(student)}> {student.name}
-                        </p>
-                        <FileCopy className="fileCopy" onClick={() => { this.copyToClip(student) }} />
+                    <div key={i} className='student-list'>
+                    <p onClick={() => this.state.selectStudent(student)}> {student.name}</p>
+                    <DialogSelect  classCode={student.studentCode}
+                                  studentName={student.name}/>
                     </div>
+
                 )
                 )}
-                <FileCopy className="fileCopy" onClick={() => { this.copyToClip() }} />
             </div>
         );
     }
