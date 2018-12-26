7ffe51a38b8efff8e01be7134fde2cbd908f5b19
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2018-12-25 18:57:48 -0600

    fixed setstate issue /nested onclicks

--- src/components/home/DisplayStudent.jsx
+++ src/components/home/DisplayStudent.jsx
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
 import _ from 'lodash';
 import { FileCopy } from '@material-ui/icons';
 
 
-const DisplayStudent = ({ students, selectStudent }) => {
-    return(
-        <div className="displayStudents">
-            { _.isEmpty(students) ? <p>No students found</p> : students.map((student, i) => (
-                <p key = {i} onClick = {() => selectStudent(student)}> {student.name} 
-                <FileCopy className="fileCopy" onClick={() => {}} />
-                </p>
-            ))}
-        </div>
-    );
+class DisplayStudent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            students: props.students,
+            selectStudent: props.selectStudent,
+            open: false,
+            something: null
+        }
+
+        this.copyToClip = this.copyToClip.bind(this);
+    }
+
+    copyToClip = (student) => {
+        console.log(student)
+
+        navigator.clipboard.writeText(`
+    1.${student.studentCode}
+    2.${student.name}
+    3. N/A
+    4. N/A 
+    `).then(() =>
+
+                this.setState({
+                    open: true,
+                    something: student.name
+                })
+            , () => alert('fail fail fail'));
+
+
+    }
+
+    render() {
+
+        return (
+            <div className="displayStudents">
+                {_.isEmpty(this.state.students) ? <p>No students found</p> : this.state.students.map((student, i) => (
+                    <div className='student-list'>
+                        <p key={i} onClick={() => this.state.selectStudent(student)}> {student.name}
+                        </p>
+                        <FileCopy className="fileCopy" onClick={() => { this.copyToClip(student) }} />
+                    </div>
+                )
+                )}
+                <FileCopy className="fileCopy" onClick={() => { this.copyToClip() }} />
+            </div>
+        );
+    }
 };
 
 export default DisplayStudent;
