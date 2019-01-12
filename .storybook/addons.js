da0993eee74b79a07cafb78ca52330a3ba81e5c3
Esterling Accime <esterling.accime@coxautoinc.com>
2019-01-12 01:55:20 -0500

    resolve conflicts

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,10 +1,5 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
-<<<<<<< HEAD
-import '@storybook/addon-knobs/register'
-import '@storybook/addon-info/'
-=======
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-info/';
 
->>>>>>> master
