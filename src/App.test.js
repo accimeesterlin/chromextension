4ad9b0692e713fe8da77fc39f5d1e949f73d679f
Esterling Accime <esterling.accime@coxautoinc.com>
2019-03-05 00:38:32 -0500

    adding node versionc

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  // global.ga = jest.fn();
+  global.ga = jest.fn();
   const div = document.createElement('div');
   ReactDOM.render(<HashRouter>
     <App />
